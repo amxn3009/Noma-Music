29c31804ee67b95a4c1a23a1d900571b47152ee6
amxn3009 <amonhose@gmail.com>
2026-09-17 22:24:48 +0200

    move to pages

--- init.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello! Your nomasmusic worker is live!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
